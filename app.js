550131426856af538f9bd1a8f03542cf423639c0
Chahir Zouhair <63002119+Dr-Hyper@users.noreply.github.com>
2021-03-04 16:52:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
+const cursor = document.querySelector('.cursor');
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
@@ -23,3 +24,33 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
+
+window.addEventListener('mousemove', (e)=>{
+	cursor.style.left = e.pageX + 'px';
+	cursor.style.top = e.pageY + 'px';
+	cursor.setAttribute('data-fromTop', (cursor.offsetTop - scrollY))
+	console.log(e.pageX, e.pageY);
+});
+
+window.addEventListener('scroll', () => {
+	const fromTop = parseInt(cursor.getAttribute('data-fromTop'));
+	cursor.style.top = scrollY + fromTop + "px";
+	console.log(scrollY);
+});
+
+window.addEventListener('click', ()=>{
+	if(cursor.classList.contains('click')){
+		cursor.classList.remove("click");
+		//trigerin g a dom refolow
+		void cursor.offsetWidth;
+		cursor.classList.add('click');
+	}else {
+		cursor.classList.add("click");
+	}
+});
+
+
+
+
+
+
