1348531d9fc192007e25bf98977e629c7433ff86
Chahir Zouhair <63002119+Dr-Hyper@users.noreply.github.com>
2023-02-14 17:06:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a'
 const header = document.querySelector('.header.container');
 const cursor = document.querySelector('.cursor');
 
+//Hamberger menu
+
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
